90a9aabef19f0ab1639dc7644eedb2812fb0056e
mini-will <apple0minimino@gmail.com>
2020-06-01 21:54:48 +0900

    [update] playinfo画面の表示項目を微修正

--- src/views/PlayInfo.vue
+++ src/views/PlayInfo.vue
@@ -5,10 +5,12 @@
         <v-col>
           <h1>play info</h1>
 
-          <p>{{ $route.params.id }}</p>
-          <p>{{ this.getPlayCardsById(Number($route.params.id)) }}</p>
+          <!-- <p>{{ this.getPlayCardsById(Number($route.params.id)) }}</p> -->
 
+          <p>{{ $route.params.id }}</p>
           <p>{{ temp[0].kanji_name }}</p>
+          <p>{{ temp[0].display_name }}</p>
+          <p>{{ temp[0].description }}</p>
         </v-col>
       </v-row>
     </v-container>

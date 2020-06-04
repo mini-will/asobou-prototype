405fe347d57961aa655c98bd2ebedbff46b40793
mini-will <apple0minimino@gmail.com>
2020-06-05 07:59:18 +0900

    [update] ランダムに表示する

--- src/views/PlayIndex.vue
+++ src/views/PlayIndex.vue
@@ -9,7 +9,42 @@
       </v-row>
 
       <h3>おうちであそぶ</h3>
-      <v-row>
+      <div v-if="!loading">
+        <v-row>
+          <v-col
+            v-for="playcard in temp3"
+            :key="playcard.id"
+            cols="6"
+            sm="3"
+            class="d-flex justify-center"
+          >
+            <v-card>
+              <v-responsive :aspect-ratio="16 / 9">
+                <v-img
+                  :src="playcard.image_url"
+                  class="white--text align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="200px"
+                >
+                  <v-card-title v-text="playcard.display_name" class="headline font-weight-bold"></v-card-title>
+                </v-img>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                  </v-btn>
+                  <v-btn>ほかの</v-btn>
+                </v-card-actions>
+              </v-responsive>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+
+      <div v-else>Loading...</div>
+
+      <!-- <v-row>
         <v-col>
           <v-card>
             <router-link :to="{ name: 'PlayInfo', params: { id:this.temp1.id } }">
@@ -25,13 +60,13 @@
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn icon>
-                <!-- <v-icon>mdi-heart</v-icon> -->
+                <v-icon>mdi-heart</v-icon>
                 <v-icon @click="setPlaydata1">mdi-cloud-upload</v-icon>
               </v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
-      </v-row>
+      </v-row>-->
 
       <h3>そとであそぶ</h3>
       <v-row>
@@ -63,44 +98,6 @@
       <!-- <p>{{ getRandom(getPlayCardsByInOut("in")).image_url }}</p>
       <p>{{ getRandom(getPlayCardsByInOut("in")) }}</p>
       <p>{{ getRandom(getPlayCardsByInOut("out")) }}</p>-->
-
-      <div v-if="!loading">
-        <!-- <v-row>
-          <v-col
-            v-for="playcard in playCards"
-            :key="playcard.id"
-            cols="12"
-            sm="6"
-            class="d-flex justify-center"
-          >
-            <v-card>
-              <v-responsive :aspect-ratio="16 / 9">
-                <v-img
-                  :src="playcard.image_url"
-                  class="white--text align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  height="200px"
-                >
-                  <v-card-title v-text="playcard.display_name" class="headline font-weight-bold"></v-card-title>
-                </v-img>
-
-                <v-card-text>
-                  <div class="text--primary">sample text .......</div>
-                </v-card-text>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn icon>
-                    <v-icon>mdi-heart</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-responsive>
-            </v-card>
-          </v-col>
-        </v-row>-->
-      </div>
-
-      <div v-else>Loading...</div>
     </v-container>
   </v-app>
 </template>
@@ -113,12 +110,19 @@ export default {
     return {
       loading: false,
       temp1: [],
-      temp2: []
+      temp2: [],
+      temp3: []
     };
   },
   created: function() {
     this.temp1 = this.getRandom(this.getPlayCardsByInOut("in"));
     this.temp2 = this.getRandom(this.getPlayCardsByInOut("out"));
+
+    this.temp3[0] = this.getRandom(this.getPlayCardsByInOut("in"));
+    this.temp3[1] = this.getRandom(this.getPlayCardsByInOut("in"));
+    this.temp3[2] = this.getRandom(this.getPlayCardsByInOut("in"));
+    this.temp3[3] = this.getRandom(this.getPlayCardsByInOut("in"));
+    this.loading = false;
   },
   computed: {
     ...mapState(["playCards"]),
